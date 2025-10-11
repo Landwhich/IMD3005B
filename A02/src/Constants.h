ca401ad3fe8310c39afe88b21466e258d27abba3
Lorelai Dusaniwsky-Eon <lorelaidusaniwsky-eon@MacBook-Air.localdomain>
2025-10-11 18:12:57 -0400

    TODO: fix addon issue w project gen

---
+++ A02/src/Constants.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Main.h"
+
+namespace Constants {
+    static const int WINDOW_WIDTH = 1280;
+    static const int WINDOW_HEIGHT = 720;
+    static const int FRAMERATE = 60;
+
+    static const std::string IMG_PATH = "happyTurtle.avif";
+}
--- A02/src/ofApp.h
+++ A02/src/ofApp.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxImGui.h"
+#include "Constant.h"
 
 class ofApp : public ofBaseApp{
 
